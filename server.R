c749d2b110bb7d19a9820e7ae2b06c7718cfb35d
wooyol <neolaocoon@naver.com>
2016-09-26 10:00:19 +0900

    edit base maps

--- server.R
+++ server.R
@@ -17,49 +17,49 @@ maxCNT <- summaryBy(OCCRRNC_LVSTCKCNT ~ LKNTS_NM, data = df, FUN = max)
 
 
 shinyServer(function(input, output, session) {
-  
+
   reactive_data <- reactive({
-    
+
     tmp <- data.table(df)
     tmp <- tmp[tmp$LKNTS_NM == input$disease & tmp$OCCRRNC_DE >= input$range[1] & tmp$OCCRRNC_DE <= input$range[2], ]
     return(tmp)
-    
+
   })
-  
+
   ncolors <- nlevels(factor(df$LVSTCKSPC_NM))
-  
+
   pal <- colorFactor(distinctColorPalette(ncolors), domain = levels(factor(df$LVSTCKSPC_NM)))
-  
-  
+
+
   output$disease <- renderUI({
-    selectInput("disease", "▷ 질병 선택",  
+    selectInput("disease", "▷ 질병 선택",
                 levels(factor(df$LKNTS_NM))
     )
   })
-  
+
   output$range <- renderUI({
-    sliderInput("range", "▷ 검색 기간",  
+    sliderInput("range", "▷ 검색 기간",
                 min = min(df$OCCRRNC_DE),
                 max = max(df$OCCRRNC_DE),
                 value = c(as.Date("20100102", "%Y%m%d"), max(df$OCCRRNC_DE))
     )
   })
-  
+
   output$Map1 <- renderLeaflet({
     leaflet() %>%
-      addTiles() %>%
-      addProviderTiles("Thunderforest.Outdoors", group = "기본") %>%
-      addProviderTiles("CartoDB.PositronNoLabels", group = "단순") %>%
       addProviderTiles("Esri.WorldImagery", group = "사진") %>%
+      addProviderTiles("CartoDB.PositronNoLabels", group = "단순") %>%
+      addProviderTiles("OpenStreetMap.HOT", group = "지명") %>%
+      addProviderTiles("Esri.WorldStreetMap", group = "도로") %>%
       setView(lng = 128, lat = 36, zoom = 6) %>%
       addLayersControl(
-        baseGroup = c("기본", "단순", "사진"),
+        baseGroup = c("사진", "단순", "지명", "도로"),
         overlayGroups = c("클러스터", "피해규모"),
         options = layersControlOptions(collapsed = FALSE)
       ) %>%
       hideGroup("피해규모")
   })
-  
+
   observe({
     leafletProxy("Map1", data = reactive_data()) %>%
       clearShapes() %>%
@@ -68,22 +68,22 @@ shinyServer(function(input, output, session) {
       addCircleMarkers(~longitude, ~latitude, clusterOptions = markerClusterOptions(), popup = ~htmlEscape(paste(OCCRRNC_DE, ",", LVSTCKSPC_NM, ",", OCCRRNC_LVSTCKCNT, "마리")), stroke = FALSE, fillOpacity = 1, color = ~pal(LVSTCKSPC_NM), group = "클러스터") %>%
       addCircles(~longitude, ~latitude, ~sqrt(OCCRRNC_LVSTCKCNT / maxCNT[maxCNT$LKNTS_NM == input$disease, ][[2]]) * 10000, popup = ~htmlEscape(paste(OCCRRNC_DE, ",", LVSTCKSPC_NM, ",", OCCRRNC_LVSTCKCNT, "마리")), stroke = FALSE, fillOpacity = 0.6, color = ~pal(LVSTCKSPC_NM), group = "피해규모")
   })
-  
+
   observe({
     input$reset
     leafletProxy("Map1") %>%
       setView(lng = 128, lat = 36, zoom = 6)
   })
-  
+
   output$diseaseName <- renderText({
     paste(input$disease, " ", input$range[1], " _ ", input$range[2])
   })
-  
+
   output$hist <- renderPlotly({
     p <- ggplot(reactive_data(), aes(LVSTCKSPC_NM)) +
       geom_bar(alpha = 0.8) +
       theme_economist() +
-      theme(axis.text.x = element_text(angle = 90)) 
+      theme(axis.text.x = element_text(angle = 90))
     gg <- ggplotly(p)
     gg %>% layout(
       title = "축종별",
@@ -91,9 +91,9 @@ shinyServer(function(input, output, session) {
       xaxis = list(title = ""),
       yaxis = list(title = "가구수")
     )
-    
+
   })
-  
+
   output$series <- renderPlotly({
     p <- ggplot(reactive_data(), aes(OCCRRNC_DE, OCCRRNC_LVSTCKCNT)) +
       geom_point(alpha = 0.15) +
@@ -106,6 +106,5 @@ shinyServer(function(input, output, session) {
       yaxis = list(title = "두수")
     )
   })
-  
-})
 
+})
